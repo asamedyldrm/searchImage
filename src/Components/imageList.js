a92495b2afaea047a76f5e4ada0c2281699fe43e
samettyldrm <smtyldrm32@gmail.com>
2023-03-27 04:03:29 +0300

    update

--- src/Components/imageList.js
+++ src/Components/imageList.js
@@ -2,7 +2,7 @@ import ImageItem from "./ImageItem";
 
 function ImageList({ imagesPlaceHolder }) {
   return  (
-  <div  className="grid grid-cols-4">
+  <div  className="grid grid-cols-4 max-sm:grid-cols-2">
     {imagesPlaceHolder.map((image, index) => {
       return <ImageItem key = {index} image={image} />;
     })}
